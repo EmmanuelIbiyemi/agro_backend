9c16ef1098b31f82ccf3c2eb6200736f52235345
Emmanuelibiyemi <ibiyemiemmanuel68@gmail.com>
2026-05-25 20:56:45 +0100

    update: cors forgot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -10,6 +11,19 @@ from modelLLM.LLM_AI import modeling
 app.include_router(auth_router)
 app.include_router(modeling)
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 @app.get("/")
 def root():
     return {
